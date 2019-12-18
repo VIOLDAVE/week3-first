ac15daca7562abfc9e0da14fbcfa02831d2a87d8
violdave <aworviola2@gmail.com>
2019-12-18 15:08:42 +0300

    made changes

---
+++ week3.js
@@ -0,0 +1,28 @@
+let arry1=[4,9.2,3,5,1,'d','a','c','f']
+function arraysort(anArray){
+    if(Array.isArray(anArray))
+{
+let obj={odds.[]}
+evens:[]
+chars:[]
+}
+for(i=o;i<anArray).length;i++){
+if (anArray[i]%2==0)
+{
+    obj.evens.push(anArray[i]);
+}
+elseif (anArray[i]%2==1){
+obj.odds.push(anArray[i]);
+    }
+else obj.char.push(anArray[i]);{
+obj.evens.sort();
+obj.odds.sort();
+obj.chars.sort();
+return obj;
+}
+else
+   { return"invalid";
+}
+}
+}
+module.exports=arraysort;
